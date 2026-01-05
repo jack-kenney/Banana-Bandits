56040e3943cb8b4bb195b6f4c8342471b682fb27
Jack Kenney <jackkenney10@gmail.com>
2026-01-05 01:15:32 +0000

    added rudimentary jumping

--- src/main.c
+++ src/main.c
@@ -7,6 +7,8 @@
 #include <t3d/t3ddebug.h>
 #include <libdragon.h>
 
+#define JUMP_HEIGHT 12.0f;
+
 surface_t *depthBuffer;
 T3DViewport viewport;
 rdpq_font_t *font;
@@ -21,11 +23,14 @@ T3DVec3 camPos;
 T3DVec3 camTarget;
 T3DVec3 lightDirVec;
 
+
 typedef struct {
     T3DVec3 moveDir;
     T3DVec3 playerPos;
     float currSpeed;
     float rotY;
+    int jumpFrame;
+    bool asc;
     rspq_block_t *dplPlayer;
     T3DMat4FP *modelMatFP;
 } Player;
@@ -41,6 +46,7 @@ void player_init(Player *player)
     player->playerPos = (T3DVec3){{0,0,0}};
     player->currSpeed = 0.0f;
     player->rotY = 0.0f;
+    player->jumpFrame = 0;
     rspq_block_begin();
         t3d_matrix_push(player->modelMatFP);
         t3d_model_draw(modelCrystal); // as in the last example, draw skinned with the main skeleton
@@ -98,6 +104,7 @@ void player_movement(Player *player, joypad_port_t port)
     player->currSpeed *= 0.64f;
     } 
     // Move player
+    if (joypad.btn.z) player->currSpeed = 5.0f;
     player->playerPos.v[0] += player->moveDir.v[0] * player->currSpeed;
     player->playerPos.v[2] += player->moveDir.v[2] * player->currSpeed;
     // ...and limit position inside the box
@@ -111,8 +118,27 @@ void player_movement(Player *player, joypad_port_t port)
     t3d_mat4fp_from_srt_euler(player->modelMatFP,
     (float[3]){0.125f, 0.125f, 0.125f},
     (float[3]){0.0f, -player->rotY, 0},
-    player->playerPos.v
-  );
+    player->playerPos.v);
+
+    if(joypad.btn.a) player->asc = true;
+
+    if (player->asc && !joypad.btn.a)
+    {
+        if(player->jumpFrame < 5)
+        {
+            player->playerPos.v[1] += JUMP_HEIGHT;
+            player->jumpFrame++;
+        }
+        else
+        {
+            player->asc = false;
+        }
+    }
+    else if (player->jumpFrame > 0)
+    {
+        player->playerPos.v[1] -= JUMP_HEIGHT;
+        player->jumpFrame--; 
+    }
 }
 
 int main(void)
